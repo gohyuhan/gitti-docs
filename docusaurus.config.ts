2dbb2b23005e8ab17a78c45ea6788ab8afa99ff3
yuhan~夕飯 ¯\_(ツ)_/¯ <gohyuhan123456@gmail.com>
2025-11-30 16:57:22 +0800

    fix: fix favicon not appearing in the browser tab

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Gitti',
   tagline: 'A fast, lightweight terminal UI for Git operations',
-  favicon: 'favicon.ico',
+  favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
